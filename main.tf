f95af664f8e4dc368579509add69b5b22b538cd8
Rolee Sinha <99283890+roleesinhaHC@users.noreply.github.com>
2022-07-07 00:36:22 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "random_pet" "pet" {
+  keepers = {
+    some_id = "90"
+  }
+  length = 100
+}
+resource "random_string" "random_suffix" {
+  length  = 38
+  special = true
+  upper   = true
+}
+resource "null_resource" "example" {}
