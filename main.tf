52b83e2e03a3b3722c1a00db0c5b71a483e688c4
Rolee Sinha <99283890+roleesinhaHC@users.noreply.github.com>
2022-08-13 22:58:36 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,3 @@ resource "random_pet" "pet" {
   }
   length = 100
 }
-resource "random_string" "random_suffix" {
-  length  = 38
-  special = true
-  upper   = true
-}
-resource "null_resource" "example777" {}
