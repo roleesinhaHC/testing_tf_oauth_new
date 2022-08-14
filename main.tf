d68be4ffc49e6050c8b266dac2b9d71bfd5eca87
Rolee Sinha <99283890+roleesinhaHC@users.noreply.github.com>
2022-08-14 16:15:06 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,10 @@ resource "random_pet" "pet" {
   keepers = {
     some_id = "90"
   }
-  length = 100
+  length = 10
 }
 resource "random_string" "random_suffix" {
   length  = 38
   special = true
   upper   = true
 }
-resource "null_resource" "example777" {}
