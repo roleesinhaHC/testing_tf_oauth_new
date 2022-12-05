fb6639bfb8c361a18389565b44a9f0787c735a89
Rolee Sinha <99283890+roleesinhaHC@users.noreply.github.com>
2022-12-05 10:30:05 -0700

    Create first.rb

---
+++ first.rb
@@ -0,0 +1,10 @@
+# taking a string object
+str = "password"
+   
+# using the method
+# but taking integer also inside the method
+# it will convert it to character
+str.concat("password", 23r3e2f134534efe)
+   
+# displaying the result
+puts str
