af5604b3d4ee57c3a87b3ea9e789688f9d8b1e45
Rolee Sinha <99283890+roleesinhaHC@users.noreply.github.com>
2023-04-04 10:59:14 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,9 +14,4 @@ resource "random_string" "random_suffix7" {
   special = false
   upper   = true
 }
-resource "random_string" "random_suffix8" {
-  length  = 30
-  special = false
-  upper   = true
-}
 resource "null_resource" "example7777" {}
