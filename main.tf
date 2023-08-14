86113f7caad84f894c5bbd3659726e861d33c111
Rolee Sinha <99283890+roleesinhaHC@users.noreply.github.com>
2023-08-14 13:47:27 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,4 @@ resource "random_pet" "pet" {
 resource "null_resource" "example7777" {}
 resource "null_resource" "example77" {}
 resource "null_resource" "example7" {}
+resource "null_resource" "example_testing" {}
