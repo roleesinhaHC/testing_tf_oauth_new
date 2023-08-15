4a41003ba656635cc508b06b60d7bd61fcf5f4fb
Rolee Sinha <99283890+roleesinhaHC@users.noreply.github.com>
2023-08-15 10:17:47 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,3 @@ resource "random_pet" "pet" {
   length = 10
 }
 resource "null_resource" "example7777" {}
-resource "null_resource" "example77" {}
-resource "null_resource" "example7" {}
-resource "null_resource" "example_testing" {}
